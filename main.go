1cd1e8194c6b3bf05890f366a287510d1905a671
cintezam <cintezam@lmax.com>
2022-01-12 21:56:57 +0000

    add yaml as a dep, start working on the idea of a config file, move arguments to a struct

--- main.go
+++ main.go
@@ -11,40 +11,68 @@ import (
 	"time"
 )
 
-var DefaultDictionary string = "/usr/share/myspell/en_US.dic"
+func configDir() string {
+	dir, err := os.UserConfigDir()
+	if nil != err {
+		log.Fatal("Could not get user config dir", err)
+	}
+	return dir + "/eldrow.yaml"
+}
 
-func main() {
-	var dict string
-	var length string
-	var skip string
-	var include string
-	var match string
-	var all bool
-	flag.StringVar(&dict, "d", DefaultDictionary, "dictionary file to use")
-	flag.StringVar(&length, "l", "*", "word length to use")
-	flag.StringVar(&skip, "s", "", "which letters to skip")
-	flag.StringVar(&include, "i", "", "which letters to include")
-	flag.StringVar(&match, "m", "", "what regular expression to match")
-	flag.BoolVar(&all, "all", false, "print all the words")
+var DefaultDictionary = "/usr/share/myspell/en_US.dic"
+
+type Args struct {
+	Dict         string
+	Length       string
+	RegexpLength string
+	Skip         string
+	Include      string
+	Match        string
+	All          bool
+}
+
+func parseArgs() Args {
+	args := Args{
+		Dict:         DefaultDictionary,
+		Length:       "*",
+		RegexpLength: "*",
+		Skip:         "",
+		Include:      "",
+		Match:        "",
+		All:          false,
+	}
+
+	flag.StringVar(&args.Dict, "d", args.Dict, "dictionary file to use")
+	flag.StringVar(&args.Length, "l", args.Length, "word length to use")
+	flag.StringVar(&args.Skip, "s", args.Skip, "which letters to skip")
+	flag.StringVar(&args.Include, "i", args.Include, "which letters to include")
+	flag.StringVar(&args.Match, "m", args.Match, "what regular expression to match")
+	flag.BoolVar(&args.All, "all", args.All, "print all the words")
 
 	flag.Parse()
 
-	file, err := os.Open(dict)
+	if "*" != args.Length {
+		args.RegexpLength = "{" + args.Length + "}"
+	}
+
+	return args
+}
+
+func main() {
+	args := parseArgs()
+
+	file, err := os.Open(args.Dict)
 
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
 	}
 
-	matcher, _ := regexp.Compile(`(?i)^` + match + `$`)
-	rlen := length
-	if "*" != rlen {
-		rlen = "{" + length + "}"
-	}
-	alpha, _ := regexp.Compile(`(?i)^[a-z]` + rlen + `$`)
+	matcher, _ := regexp.Compile(`(?i)^` + args.Match + `$`)
+	alpha, _ := regexp.Compile(`(?i)^[a-z]` + args.RegexpLength + `$`)
 
 	var includes []rune
-	if "" != include {
-		includes = []rune(include)
+	if "" != args.Include {
+		includes = []rune(args.Include)
 	}
 
 	scanner := bufio.NewScanner(file)
@@ -57,20 +85,20 @@ func main() {
 			word = strings.Split(word, "/")[0]
 		}
 		ok := true
-		if "" != match {
+		if "" != args.Match {
 			ok = matcher.MatchString(word)
 		} else {
 			ok = alpha.MatchString(word)
 		}
 
-		if ok && "" != include {
+		if ok && "" != args.Include {
 			for _, r := range includes {
 				ok = ok && strings.ContainsRune(word, r)
 			}
 		}
 
-		if ok && "" != skip {
-			ok = !strings.ContainsAny(word, skip)
+		if ok && "" != args.Skip {
+			ok = !strings.ContainsAny(word, args.Skip)
 		}
 
 		if ok {
@@ -80,19 +108,19 @@ func main() {
 
 	_ = file.Close()
 
-	if "" == skip && "" == include && "" == match {
-		if all {
-			log.Printf("All words of length %s:", length)
+	if "" == args.Skip && "" == args.Include && "" == args.Match {
+		if args.All {
+			log.Printf("All words of length %s:", args.Length)
 			for _, word := range words {
 				log.Printf("\t%s", word)
 			}
 		} else {
 			rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 			inx := rng.Intn(len(words))
-			log.Printf("Returning a random %s letter word: %s", length, words[inx])
+			log.Printf("Returning a random %s letter word: %s", args.Length, words[inx])
 		}
 	} else {
-		log.Printf("Words that match the restrictions [length: %s, include: '%s', skip: '%s', match: '%s']:", length, include, skip, match)
+		log.Printf("Words that match the restrictions [length: %s, include: '%s', skip: '%s', match: '%s']:", args.Length, args.Include, args.Skip, args.Match)
 		for _, word := range words {
 			log.Printf("\t%s", word)
 		}
